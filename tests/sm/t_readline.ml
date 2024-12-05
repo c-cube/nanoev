a1dfe51cabdaa4bebe260ab0cfaed7900258070d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-12-04 23:22:45 -0500

    rename to nanoev.sm

--- tests/sm/t_readline.ml
+++ tests/sm/t_readline.ml
@@ -1,4 +1,4 @@
-module CPS = Nanoev_cps
+module SM = Nanoev_sm
 
 let spf = Printf.sprintf
 let pf = Printf.printf
@@ -39,30 +39,30 @@ type input =
   | `Input of bytes * int * int
   ]
 
-let readline () : ([ `Yield of string ], input, unit, CPS.void) CPS.t =
+let readline () : ([ `Yield of string ], input, unit, SM.void) SM.t =
   let self = St.create () in
-  CPS.create_rec @@ fun ~aq ~recurse ->
+  SM.create_rec @@ fun ~aq ~recurse ->
   match
     for i = 0 to self.len - 1 do
       if Bytes.get self.bs i = '\n' then raise_notrace (Found i)
     done
   with
   | () ->
-    CPS.await @@ fun input ->
+    SM.await @@ fun input ->
     (match input with
-    | `End_of_input when self.len = 0 -> CPS.return ()
+    | `End_of_input when self.len = 0 -> SM.return ()
     | `End_of_input ->
       let line = Bytes.sub_string self.bs 0 self.len in
       St.shift_left self self.len;
-      CPS.perform ~aq (`Yield line);
-      CPS.return ()
+      SM.perform ~aq (`Yield line);
+      SM.return ()
     | `Input (buf, i, len) ->
       St.add_bytes self buf i len;
       recurse ())
   | exception Found i ->
     let line = Bytes.sub_string self.bs 0 i in
     St.shift_left self (i + 1);
-    CPS.perform ~aq (`Yield line);
+    SM.perform ~aq (`Yield line);
     recurse ()
 
 let test (inputs : string list) =
@@ -72,21 +72,21 @@ let test (inputs : string list) =
   let rd = readline () in
 
   let work () =
-    (match CPS.work rd with
+    (match SM.work rd with
     | Ok () -> pf "done\n"
     | Error _ -> .
     | Working -> ());
-    CPS.pop_actions rd (function `Yield line -> pf "yielded line %S\n%!" line)
+    SM.pop_actions rd (function `Yield line -> pf "yielded line %S\n%!" line)
   in
 
   work ();
   while not (Queue.is_empty inputs) do
     let s = Queue.pop inputs in
     pf "feed input %S\n%!" s;
-    CPS.add_input rd (`Input (Bytes.of_string s, 0, String.length s));
+    SM.add_input rd (`Input (Bytes.of_string s, 0, String.length s));
     work ()
   done;
-  CPS.add_input rd `End_of_input;
+  SM.add_input rd `End_of_input;
   work ();
 
   pf "## end test\n"
