8de69367875c3bf5caab86f410ec84a32fd6bf69
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-04-30 21:46:03 -0400

    format

--- src/picos/nanoev_picos.mli
+++ src/picos/nanoev_picos.mli
@@ -4,29 +4,33 @@ val setup_bg_thread : Nanoev.t -> unit
 (** Install this event loop in a background thread *)
 
 val has_bg_thread : unit -> bool
-(** [has_bg_thread ()] is [true] iff a background thread is running a nanoev loop *)
+(** [has_bg_thread ()] is [true] iff a background thread is running a nanoev
+    loop *)
 
 (** {2 Non blocking IO primitives} *)
 
 val read : Unix.file_descr -> bytes -> int -> int -> int
 (** Read from the non blocking FD.
-  @raise Nanoev.Closed if the FD is closed
-  @raise Unix.Unix_error for other errors *)
+    @raise Nanoev.Closed if the FD is closed
+    @raise Unix.Unix_error for other errors *)
 
 val write : Unix.file_descr -> bytes -> int -> int -> int
 (** Write into the non blocking FD.
-  @raise Nanoev.Closed if the FD is closed
-  @raise Unix.Unix_error for other errors *)
+    @raise Nanoev.Closed if the FD is closed
+    @raise Unix.Unix_error for other errors *)
 
 val close : Unix.file_descr -> unit
 (** Close the file descriptor
-  @raise Unix.Unix_error when it fails *)
+    @raise Unix.Unix_error when it fails *)
 
 val connect : Unix.file_descr -> Unix.sockaddr -> unit
+(** Connect this FD to the remote address.
+    @raise Nanoev.Closed if the FD is closed.
+    @raise Unix.Unix_error for other errors *)
 
 val accept : Unix.file_descr -> Unix.file_descr * Unix.sockaddr
 (** Accept a connection on this fd.
-  @raise Nanoev.Closed if the FD is closed.
-  @raise Unix.Unix_error for other errors *)
+    @raise Nanoev.Closed if the FD is closed.
+    @raise Unix.Unix_error for other errors *)
 
 val sleep : float -> unit
